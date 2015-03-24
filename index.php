2faf73cacd0ba07c7f9a0b7cf283ca2e03f72676
jpr <jpr@10.0.1.7>
2015-03-24 10:46:53 -0300

    here is more dev msg

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-       print 'this is the removal of test';
+       print 'new dev branch';
         ?>
     </body>
 </html>
