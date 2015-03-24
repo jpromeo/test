c0a1aed825124920a4a6a6ba81e4ad11734b9369
jpr <jpr@10.0.1.7>
2015-03-24 10:44:25 -0300

    here is another msg

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-       print 'here is the test';
+       print 'this is the removal of test';
         ?>
     </body>
 </html>
